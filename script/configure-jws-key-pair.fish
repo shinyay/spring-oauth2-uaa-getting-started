180ad78e434a1ba5ad55452966c680f052d0f161
shinyay <shinya.com@gmail.com>
2019-10-20 18:56:27 +0900

    Add a script for JWS Token in uaa.xml

--- script/configure-jws-key-pair.fish
+++ script/configure-jws-key-pair.fish
@@ -1,11 +1,7 @@
 #!/usr/bin/env fish
 
-if test -e signingkey.pem
-  openssl genrsa -out signingkey.pem 2048
-end
-if test -e verificationkey.pem
-  openssl rsa -in signingkey.pem -pubout -out verificationkey.pem
-end
+openssl genrsa -out signingkey.pem 2048
+openssl rsa -in signingkey.pem -pubout -out verificationkey.pem
 
 set -x JWT_TOKEN_SIGNING_KEY (cat signingkey.pem)
 set -x JWT_TOKEN_VERIFICATION_KEY (cat verificationkey.pem)
