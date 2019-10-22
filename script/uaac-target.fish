874bf9625c10f14a6fced85fddf8451e27052d7c
shinyay <shinya.com@gmail.com>
2019-10-22 17:48:41 +0900

    Modify command option which uses local directory volume attached

--- script/uaac-target.fish
+++ script/uaac-target.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-docker run --rm -it shinyay/cf-uaac target http://(ifconfig en0 | awk '/inet / {print $2}'):8080/uaa
+docker run --rm -v (pwd):/root -it shinyay/cf-uaac target http://(ifconfig en0 | awk '/inet / {print $2}'):8080/uaa
