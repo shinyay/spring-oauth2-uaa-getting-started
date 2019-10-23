436d8cf8cbbc6d3e696aa08fa081e60afe632850
shinyay <shinya.com@gmail.com>
2019-10-23 09:19:26 +0900

    Add script message for explanation

--- script/uaac-target.fish
+++ script/uaac-target.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
+echo "[TARGET UAA SERVER LOCATION]"
 docker run --rm -v (pwd):/root -it shinyay/cf-uaac target http://(ifconfig en0 | awk '/inet / {print $2}'):8080/uaa
--- script/uaac-token.fish
+++ script/uaac-token.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
+echo "[RETRIEVE TOKEN FROM UAA SERVER]"
 docker run --rm -v (pwd):/root -it shinyay/cf-uaac token client get admin -s adminsecret
