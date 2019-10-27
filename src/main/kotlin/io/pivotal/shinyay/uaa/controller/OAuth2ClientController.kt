8f49ad4e18cd388f4db0f6f8283e8331066ac749
shinyay <shinya.com@gmail.com>
2019-10-27 16:11:23 +0900

    Add Access Token Retrieval from Authorization Server, UAA

--- src/main/kotlin/io/pivotal/shinyay/uaa/controller/OAuth2ClientController.kt
+++ src/main/kotlin/io/pivotal/shinyay/uaa/controller/OAuth2ClientController.kt
@@ -1,11 +1,16 @@
 package io.pivotal.shinyay.uaa.controller
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
 @RestController
@@ -22,7 +27,7 @@ class OAuth2ClientController(@Value("\${resource.server.url}") val remoteResourc
                                 authenticationToken.name)
         val oAuth2AccessToken = oAuth2AuthorizedClient.accessToken
 
-        val message = "Hello, ${authenticationToken.principal.name}" +
+        return "Hello, ${authenticationToken.principal.name}" +
                 "</br></br>" +
                 "Here is your access token :</br>" +
                 "${oAuth2AccessToken.tokenValue}</br>" +
@@ -30,7 +35,32 @@ class OAuth2ClientController(@Value("\${resource.server.url}") val remoteResourc
                 "<a href='/read'>Call Resource Server Read API</a>" +
                 "</br>" +
                 "<a href='/write'>Call Resource Server Write API</a>"
-        return message
+    }
+
+    fun retrieveAccessToken(authenticationToken: OAuth2AuthenticationToken, url: String): String {
+        val oAuth2AuthorizedClient: OAuth2AuthorizedClient =
+                this.authorizedClientService
+                        .loadAuthorizedClient(
+                                authenticationToken.authorizedClientRegistrationId,
+                                authenticationToken.name
+                        )
+        val oAuth2AccessToken = oAuth2AuthorizedClient.accessToken
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.add("Authorization", "Bearer " + oAuth2AccessToken.tokenType)
+
+        val httpEntity: HttpEntity<String> = HttpEntity("parameters", httpHeaders)
+        val responseEntity: ResponseEntity<String>
+
+        var response: String
+
+        try {
+            responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String::class.java)
+            response = responseEntity.body!!
+        } catch (e: HttpClientErrorException) {
+            response = e.message!!
+        }
+        return response
     }
 }
 
